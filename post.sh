9f215cd0bd5f6555fa8312a94e60c2bab62320bf
Ray <linxray@gmail.com>
2014-03-26 16:26:32 +0800

    add post scripts

---
+++ post.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+scriptdir=$(dirname $0)
+if [ -z "$1" ]; then
+  echo "title required"
+  exit 1
+fi
+
+d=$(date "+%Y-%m-%d-")
+time=$(date "+%Y-%m-%d %H:%M:%S")
+targetfile=${scriptdir}/src/documents/article/${d}${1}.html.md
+
+touch ${targetfile} || exit 1
+cat > ${targetfile} <<EOF
+---
+layout: post
+title: 
+tags: 
+date: #date#
+udate: #date#
+category: 
+---
+EOF
+sed -i "s/#date#/${time}/g" ${targetfile} && vi ${targetfile}
+exit 0
